3c55e7cbdc07569aed929a4b8afdb3aa94105c03
Michael Kennedy <mikeckennedy@gmail.com>
2022-12-20 09:51:55 -0800

    Better error messages

--- code/ch02-error-messages/better_messages.py
+++ code/ch02-error-messages/better_messages.py
@@ -21,7 +21,7 @@ def main():
             "country": {
                 "size": {
                     "area": 1_000_000,
-                    "units": "miles   ",
+                    "units": "miles",
                 }
             }
         }
@@ -34,24 +34,28 @@ def main():
 
     print(func1(data, b, c, d, e))
 
-    # d = "other"
+    # d = "bigness"
     # print(func1(data, b, c, d, e))
 
     t1 = Thing("Bob", 70, 24)
     t2 = Thing("Sarah", 65, 32)
     t3 = Thing("Jake", 72, 20)
 
-    total = func2(t1, t2, t3)
-    print(f'The total weight is {total:,}.')
+    total_weight = func2(t1, t2, t3)
+    print(f'The total weight is {total_weight} kg')
 
     # t2 = None
-    # total = func2(t1, t2, t3)
-    # print(f'The total weight is {total:,}.')
+    # total_weight = func2(t1, t2, t3)
+    # print(f'The total weight is {total_weight} kg')
 
-    # print(t1.weight + t4.weight)
-    # NameError: name 't4' is not defined. Did you mean: 't1'?
+    print(func3(1, 2, 3, 4))
+    # print(func3(1, 2, "3", 4))
 
-    # print(f'Sum is {func3(1, 2, "3", 4)}')
+    sarah = t2
+    bob = t1
+
+    # print(f"The weight of bob and sarah is {bob.weight} & {sara.weight}")
+
 
 
 if __name__ == '__main__':
