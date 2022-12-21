817e333ca2d8a0eacee453547b6e69d6168b1785
Michael Kennedy <mikeckennedy@gmail.com>
2022-12-20 17:07:31 -0800

    Starter for except*

---
+++ code/ch04-error-handling/except-star.py
@@ -0,0 +1,81 @@
+import asyncio
+import random
+
+from asyncio import TaskGroup
+from typing import Any
+
+
+# region Async methods for logging
+
+async def record_action_to_db():
+    print("Logging to DB...")
+    # noinspection PyTypeChecker
+    outcomes: list[Any] = [None] * 3 + [DbException] * 2 + [SocketException]
+
+    if error := random.choice(outcomes):
+        raise error("No can DB!")
+
+    print("Logged to DB successfully.")
+
+
+async def record_action_to_external_api():
+    print("Logging to API...")
+    # noinspection PyTypeChecker
+    outcomes: list[Any] = [None] * 2 + [HttpException] * 3
+
+    if error := random.choice(outcomes):
+        raise error("No can API!")
+
+    print("Logged to API successfully.")
+
+
+async def record_action_to_log():
+    print("Logging to File...")
+    # noinspection PyTypeChecker
+    outcomes: list[Any] = [None] * 3 + [FileException] * 2
+
+    if error := random.choice(outcomes):
+        raise error("No can File!")
+
+    print("Logged to File successfully.")
+
+
+# endregion
+
+# region Local exception types
+class DbException(Exception):
+    ...
+
+
+class HttpException(Exception):
+    ...
+
+
+class SocketException(Exception):
+    ...
+
+
+class FileException(Exception):
+    ...
+
+
+# endregion
+
+
+def main():
+    try:
+        asyncio.run(record_action())
+    except Exception as x:
+        print("Error:")
+        print(x)
+
+
+async def record_action():
+    async with TaskGroup() as tg:
+        tg.create_task(record_action_to_external_api())
+        tg.create_task(record_action_to_log())
+        tg.create_task(record_action_to_db())
+
+
+if __name__ == '__main__':
+    main()
